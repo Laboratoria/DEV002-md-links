e57d59980a836debd6fd4b5e45227e11cdf37454
Sharon Arana <shar_mdza@hotmail.com>
2022-12-05 12:52:02 -0600

    Añadiendo ejemplo de promesa

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
+/* module.exports = () => {
   // ...
-};
+}; */
+
+/* const links = 0
+
+const countLinks = new Promise(function(resolve, reject) {
+  if(links >= 1){
+    resolve(`Hay ${links} links en la carpeta`)
+  } else {
+    reject('No hay links en la carpeta')
+  }
+})
+
+countLinks.then((resultado) => {
+  console.log(resultado)
+}).catch((error) => {
+  console.log(error)
+}).finally(() => console.log('Fin de la ejecución')) */
