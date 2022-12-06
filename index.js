1ea44b7e1eca9fe534018b0e3450e2f357236f28
Sharon Arana <shar_mdza@hotmail.com>
2022-12-05 18:41:00 -0600

    Añadiendo lógica para tomar un archivo, leerlo y que retorne un array con los links encontrados

--- index.js
+++ index.js
@@ -2,18 +2,26 @@
   // ...
 }; */
 
-/* const links = 0
+const fs = require("fs");
+const reExp = /\[([^\]]+)\]\(([^)]+)\)/g
+const filePath = './files/preambulo.md'
 
-const countLinks = new Promise(function(resolve, reject) {
-  if(links >= 1){
-    resolve(`Hay ${links} links en la carpeta`)
-  } else {
-    reject('No hay links en la carpeta')
-  }
+const findLinks = new Promise(function(resolve, reject){
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    const links = data.match(reExp)
+    if(links){
+      resolve(`hay links ${links}`)
+    } else {
+      reject('no hay links')
+
+    }
+  })
 })
 
-countLinks.then((resultado) => {
+findLinks.then((resultado) => {
   console.log(resultado)
 }).catch((error) => {
   console.log(error)
-}).finally(() => console.log('Fin de la ejecución')) */
+}).finally(() => console.log('Fin de la ejecución'))
+
