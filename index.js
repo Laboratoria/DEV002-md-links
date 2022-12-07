e2cb877b6ce7a997fffc980ad6bcadeed8338e0e
filletournesols <dgalarcegonzalez@gmail.com>
2022-12-07 10:04:25 -0300

    función mdLink

--- index.js
+++ index.js
@@ -1,58 +1,93 @@
-//node index.js
+const { statFun, readDirFun, readFileFun } = require("./data.js")
 
-module.exports = () => {
-  // ...
-};
+const path = require("path");
+const isValid = require("is-valid-path");
+const markdownLinkExtractor = require("markdown-link-extractor");
 
-const fs = require('fs');
-const path = require('path');
+const ruta = "./mdtests"
+const rutaPart1 = "./mdtests/part1.md"
+const rutaPart2 = "./mdtests/part2.md"
+const rutaPart3 = "./mdtests/part3.md"
+const rutaPart4 = "./mdtests/part4.md"
 
-const mdLinks = (readingFile) => {
-  return new Promise((resolve, reject) => {
-  const fileString = readingFile.toString()
-  //console.log("fileString: " + fileString)
-  fs.readdir(fileString, (err, files) => {
-    if (err){
-      //console.log("err 1: " + err);
-      reject(err)
-    }
-    else {
-      const readingFiles = files.map((file)=>{
-        return new Promise((done, notDone)=>{
-        const filePaths = path.join(fileString, file)
-        //console.log("filePaths: " + filePaths)
-        const fileExt = path.extname(filePaths)
-        //console.log("fileExt: " + fileExt)
-        if (fileExt === ".md"){
-            const justMD = filePaths.slice(".md").split(",")
-            //console.log("justMD: " + justMD)
-            const mdString = justMD.toString()
-            //console.log("mdString: " + mdString)
-            const resolvePath = path.resolve(mdString)
-            //console.log("resolvePath: " + resolvePath)
-            fs.readFile(resolvePath, (err, data) => {
-              if (err) {
-                //console.error("error 2: " + err);
-                notDone(err);
-              } else {
-                //console.log("data:\n" + data)
-                done(data)
-              }
-            })
-          }
-        })
-      })
-      resolve(readingFiles)
-    }
-  })
-  })
-}
-
-mdLinks('./mdtests').then((readingFiles) => {
-  const values = Promise.all(readingFiles)
-  // Promise.all(readingFiles).then(values => {
-  //   console.log("values; ", values)
-  // })
-  console.log("filesPromise: ", values)
+/* lee función statFun y contiene validación de si es directorio o archivo */
+const isDirectory = (ruta) => new Promise((resolve, reject) => {
+    statFun(ruta).then((stats)=>{
+        /* ve si es archivo */
+        //const isFile = stats.isFile()
+        //console.log("isFile: ", isFile)
+        /* ve si es carpeta */
+        const isDirectory = stats.isDirectory()
+        //console.log("isDirectory: ", isDirectory)
+        resolve(isDirectory)
+        //console.log("isDirectory: ", isDirectory)
+    })
 })
-//console.log("function:\n" , mdLinks('./mdtests'))
+
+/* console.log de readDirFun, también sirve para utilizar la función */
+/**
+ * 
+ * @param {string} ruta 
+ * @returns Promise with the result of all the md files inside the route
+ */
+const onlyMdFiles = (ruta) => new Promise((resolve, reject) => {
+    readDirFun(ruta).then((readDirFunFiles)=> {
+        resolve(readDirFunFiles.map((file)=>{
+            const fileExt = path.extname(file)
+            if (fileExt === ".md") {
+                const justMD = file.slice(".md").split(",")
+                //console.log("justMD: ", justMD)
+                const mdString = justMD.toString()
+                //console.log("mdString: ", mdString)
+                /* ruta para carpeta */
+                const joinPaths = path.join(ruta, mdString)
+                //console.log("joinPaths: ", joinPaths)
+                const resolvePaths = path.resolve(joinPaths)
+                //console.log("resolvePath: ", resolvePaths)
+                const resolvePathsString = resolvePaths.toString()
+                //console.log("resolvePathsString: ", resolvePathsString)
+                return resolvePathsString
+            } else {
+                return null
+            }
+        }).filter((value)=> value))
+    })
+})
+
+
+const getLinksFromMdFile = (file) => new Promise((resolve, reject) => {
+    readFileFun(file).then((gettingLinks)=>{
+        //console.log("gettingLinks: ", gettingLinks)
+        const { links } = markdownLinkExtractor(gettingLinks);
+        resolve(links)
+        // links.forEach(link => {
+        //     console.log("link: ", link)
+        // });
+    })
+})
+
+const getLinksFromMdFiles = (mdFiles) => new Promise((resolve, reject) => {
+    const promises = mdFiles.map((element) => getLinksFromMdFile(element))
+    resolve(Promise.all(promises))
+})
+
+const mdLink = (rutita) => new Promise((resolve, reject) => {
+    if (!isValid(rutita)) reject("INVALID PATH");
+    //console.log("rutita: ", rutita)
+    isDirectory(rutita).then(isFolder => {
+        if (isFolder) {
+            onlyMdFiles(rutita).then(files => {
+                //console.log("files: ", files)
+                getLinksFromMdFiles(files).then(links => resolve(links))
+            })
+        } else {
+            getLinksFromMdFile(rutita).then(links => {
+                //console.log("links: ", links)
+                resolve(links)
+            })
+        }
+    })  
+})
+
+mdLink(ruta).then(console.log)
+// mdLink(rutaPart1)
--- index2.js
+++ index2.js
@@ -1,30 +1,58 @@
+//node index.js
+
+module.exports = () => {
+  // ...
+};
+
 const fs = require('fs');
 const path = require('path');
 
 const mdLinks = (readingFile) => {
-return fs.readdirSync(readingFile).map(file => {
-const filePaths = path.join(readingFile, file)
-  //console.log("filePaths: " + filePaths)
-const fileExt = path.extname(filePaths)
-  //console.log("fileExt: " + fileExt)
-if (fileExt === ".md"){
-    const justMD = filePaths.slice(".md").split(",")
-    //console.log("justMD: " + justMD)
-    const mdString = justMD.toString()
-    //console.log("mdString: " + mdString)
-    const resolvePath = path.resolve(mdString)
-    //console.log("resolvePath: ", resolvePath)
-    return fs.readFile(resolvePath, (err, data) => {
-    if (err) {
-        //console.error("error: " + err);
-        return err;
-    } else {
-        //console.log("data:\n" + data)
-        return data
+  return new Promise((resolve, reject) => {
+  const fileString = readingFile.toString()
+  //console.log("fileString: " + fileString)
+  fs.readdir(fileString, (err, files) => {
+    if (err){
+      //console.log("err 1: " + err);
+      reject(err)
     }
-    }) 
-}
-})
+    else {
+      const readingFiles = files.map((file)=>{
+        return new Promise((done, notDone)=>{
+        const filePaths = path.join(fileString, file)
+        //console.log("filePaths: " + filePaths)
+        const fileExt = path.extname(filePaths)
+        //console.log("fileExt: " + fileExt)
+        if (fileExt === ".md"){
+            const justMD = filePaths.slice(".md").split(",")
+            //console.log("justMD: " + justMD)
+            const mdString = justMD.toString()
+            //console.log("mdString: " + mdString)
+            const resolvePath = path.resolve(mdString)
+            //console.log("resolvePath: " + resolvePath)
+            fs.readFile(resolvePath, (err, data) => {
+              if (err) {
+                //console.error("error 2: " + err);
+                notDone(err);
+              } else {
+                //console.log("data:\n" + data)
+                done(data)
+              }
+            })
+          }
+        })
+      })
+      resolve(readingFiles)
+    }
+  })
+  })
 }
 
-console.log("function:\n" , mdLinks('./mdtests'))
+mdLinks('./mdtests').then((readingFiles) => {
+  const values = Promise.all(readingFiles)
+  // Promise.all(readingFiles).then(values => {
+  //   console.log("values; ", values)
+  // })
+  console.log("filesPromise: ", values)
+})
+//console.log("function:\n" , mdLinks('./mdtests'))
