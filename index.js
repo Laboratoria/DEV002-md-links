c8af1e310c72f386055fc8c3775f3a59190d03e6
Sharon Arana <shar_mdza@hotmail.com>
2022-12-07 13:26:55 -0600

    Añadiendo validación de si el path es una carpeta o un archivo

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
   // ...
 }; */
 
+
+
+/* Leer archivos */
 const fs = require("fs");
 const reExp = /\[([^\]]+)\]\(([^)]+)\)/g
-const filePath = './files/preambulo.md'
+const path = './files/preambulo.md'
 
 const findLinks = new Promise(function(resolve, reject){
 
-  fs.readFile(filePath, "utf8", (err, data) => {
+  fs.readFile(path, "utf8", (err, data) => {
     const links = data.match(reExp)
     if(links){
       resolve(`hay links ${links}`)
     } else {
       reject('no hay links')
-
     }
   })
 })
@@ -25,3 +27,20 @@ findLinks.then((resultado) => {
   console.log(error)
 }).finally(() => console.log('Fin de la ejecución'))
 
+/* Validar si es o no un directorio*/
+const isDirectoryOrFile = new Promise(function(resolve, reject){
+
+  fs.stat(path, (err, data) => {
+    if(data.isDirectory()){
+      resolve(`Soy una carpeta`)
+    } else {
+      reject(`Soy un archivo`)
+    }
+  })
+})
+
+isDirectoryOrFile.then((resultado) => {
+  console.log(resultado)
+}).catch((error) => {
+  console.log(error)
+}).finally(() => console.log('Fin de la ejecución'))
