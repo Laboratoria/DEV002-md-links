d91da74652c54ac5f8721a40841fb313275c8ec9
Sharon Arana <shar_mdza@hotmail.com>
2022-12-07 18:18:16 -0600

    Uniendo si es un directorio/ archivo con si hay archivos .md en el directorio

--- index.js
+++ index.js
@@ -6,11 +6,12 @@
 const fs = require("fs");
 const reExp = /\[([^\]]+)\]\(([^)]+)\)/g
 const path = require('path')
-const route = './files'
+const route = './files/preambulo.md'
+const mdFiles = []
 
 /* const findLinks = new Promise(function(resolve, reject){
 
-  fs.readFile(route, "utf8", (err, data) => {
+  fs.readFile(route, "utf8", (e, data) => {
     const links = data.match(reExp)
     if(links){
       resolve(`hay links ${links}`)
@@ -27,43 +28,37 @@ findLinks.then((resultado) => {
 }).finally(() => console.log('Fin de la ejecución')) */
 
 /* Validar si es o no un directorio*/
-/* const isDirectoryOrFile = new Promise(function(resolve, reject){
+const isDirectoryOrFile = new Promise(function(resolve, reject){
 
-  fs.stat(route, (err, data) => {
+  fs.stat(route, (e, data) => {
     if(data.isDirectory()){
-      resolve(`Soy una carpeta`)
+      resolve(isMarkDown)
     } else {
       reject(`Soy un archivo`)
     }
   })
 })
 
-isDirectoryOrFile.then((resultado) => {
-  console.log(resultado)
-}).catch((error) => {
-  console.log(error)
-}).finally(() => console.log('Fin de la ejecución')) */
-
 /* Buscar los archivos .md en un directorio*/
 const isMarkDown = new Promise(function(resolve, reject){
-  const mdFiles = []
 
   fs.readdir(route, (e, data) => {
-    data.forEach(item => {
+
+    data?.forEach(item => {
       if(path.extname(item) == '.md'){
         mdFiles.push(item)
       }
     })
 
     if(mdFiles.length >= 1){
-      resolve('si hay archivos .md' + mdFiles)
+      resolve('si hay archivos mark down: ' + mdFiles)
     } else {
       reject('no hay archivos para mostrar')
     }
   })
 })
 
-isMarkDown.then((resultado) => {
+Promise.allSettled([isDirectoryOrFile, isMarkDown]).then((resultado) => {
   console.log(resultado)
 }).catch((error) => {
   console.log(error)
