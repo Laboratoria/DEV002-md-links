ea045877b8d22bef05bf2b322c19b497a7b68a70
Kamila Ojeda <beau.ojeda@gmail.com>
2022-12-30 10:59:46 -0500

    Exporta md_links y se llama a ejecutar en el archivo cli.js.

--- cli.js
+++ cli.js
@@ -1,46 +1,13 @@
 const process = require("process")
-const { getLinksFromFileOrDirectoryPromise,
-  validateArrayLinksPromise,
-  countUniqueLinks,
-  getOptionsFromArguments,
-  getPathFromArguments } = require("./md_links.js")
+const { mdLinks } = require("./index.js")
+const {getPathFromArguments, getOptionsFromArguments} = require("./md_links")
 
 const arguments = process.argv;
-
-const mdLinks = (entryPath, arrayOptions) => {
-  return new Promise((resolve, reject) => {
-    if (arrayOptions.includes("--validate") && arrayOptions.includes("--stats")) {
-      getLinksFromFileOrDirectoryPromise(entryPath)
-        .then(links => validateArrayLinksPromise(links))
-        .then(arrayObjects => resolve(
-          "Total links: " + arrayObjects.length + "\n" +
-          "Unique: " + countUniqueLinks(arrayObjects) + "\n" +
-          "Broken: " + arrayObjects.filter(link => link.message === "Fail").length
-        ))
-        .catch(error => reject(error))
-    } else if (arrayOptions.includes("--validate")) {
-      getLinksFromFileOrDirectoryPromise(entryPath)
-        .then(links => validateArrayLinksPromise(links))
-        .then(arrayObjects => resolve(arrayObjects))
-        .catch(error => reject(error))
-    } else if (arrayOptions.includes("--stats")) {
-      getLinksFromFileOrDirectoryPromise(entryPath)
-        .then(arrayLinks => resolve(
-          "Total links: " + arrayLinks.length + "\n" +
-          "Unique links: " + countUniqueLinks(arrayLinks)
-        ))
-        .catch(error => reject(error))
-    } else if (arrayOptions.length == 0) {
-      getLinksFromFileOrDirectoryPromise(entryPath)
-        .then(res => resolve(res))
-        .catch(error => reject(error))
-    }
-
-  })
-}
-
-module.exports = { mdLinks }
 
 mdLinks(getPathFromArguments(arguments), getOptionsFromArguments(arguments))
   .then(response => console.log(response))
   .catch(error => console.log(error));
+
+module.exports = { mdLinks }
+
+
