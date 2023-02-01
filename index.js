29a666efddae4cfe1348dd2941e24df908000f48
Melanie Tolvett S <melanietolvett@gmail.com>
2023-02-01 10:31:10 -0300

    agregando archivo para test

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-module.exports = () => {
-  // ...
-};
+// module.exports = () => {
+//   //
+// };
+
+  const fs = require('fs');
+
+  //console.log(fs);
+
+  //---------Leyando un archivo md con ruta fija
+  const readFileFn = (path) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (!error) {
+        console.log(data);
+      } else {
+        console.log(`Error: ${error}`);
+      }
+    });
+  };
+  readFileFn('README.md');
+
+module.exports = {readFileFn}
+
+
