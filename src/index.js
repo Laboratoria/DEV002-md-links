99a7ba9a7c1df6e421d5acf6d15ca1c4ae3897ff
Melanie Tolvett S <melanietolvett@gmail.com>
2023-02-03 22:59:16 -0300

    configurando package.json

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const mdLinks = (path, option) => {
+
+// validación de path
+const validatePath = new Promise ((resolve, reject) => {
+    if ('ruta valida') {
+        resolve(true)
+    } else {
+        reject(false)
+    }
+})
+
+// validación de option
+// validación de path absoluta o relativa
+// validación de directorio
+// validación de archivo md
+
+}
+
+module.exports = { mdLinks }
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,5 +1,4 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../src/index.js');
 
 describe('mdLinks', () => {
 
@@ -8,3 +7,19 @@ describe('mdLinks', () => {
   });
 
 });
+
+// test para validacion de path
+
+describe('Test para validar la ruta (path)', () => {
+  it('Retorna true si la ruta es valida', () => {
+    expect(validatePath(path)).resolve.toEqual(true);
+  });
+  it('Retorna false si la ruta es invalida', () => {
+    expect(validatePath(path)).reject.toEqual(false);
+  });
+});
+
+// test para validación de option
+// test para validación de path absoluta o relativa
+// test para validación de directorio
+// test para validación de archivo md
