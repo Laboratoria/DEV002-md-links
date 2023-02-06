4bc63d01d3ca5d5cfff6f82303fbd719b43b733f
Melanie Tolvett S <melanietolvett@gmail.com>
2023-02-06 15:03:41 -0300

    nueva rama

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
+const fs = require('fs');
+
 const mdLinks = (path, option) => {
+    console.log('fix me')
+};
 
-// validación de path
-const validatePath = new Promise ((resolve, reject) => {
-    if ('ruta valida') {
-        resolve(true)
-    } else {
-        reject(false)
-    }
-})
 
-// validación de option
+// Validación de ruta: Check if the file exists in the current directory and is readeble.
+const validatePath = (path) => {
+    return new Promise((resolve, reject) => {
+        fs.access(path, fs.constants.F_OK | fs.constants.R_OK, (err) => {
+            if (!err) {
+                resolve(true)
+            } else {
+                reject(new Error('Invalid or unreadeble path'))
+            }
+        })
+    })
+};
+
+/* validatePath('./files-to-read/achicando.md')
+    .then(res => console.log(res))
+    .catch(err => console.error(err)) */
+
+// validación de option | 'validate', {validate : true}, {validate : false}
+const validateOption = (option) => {
+    return new Promise((resolve, reject) => {
+        if ( option === 'validate' || (typeof option === 'object' && (option.validate === true || option.validate === false)) ) {
+            resolve(true)
+        } else {
+            reject(new Error('Invalid option'))
+        }
+    })
+};
+
+
+
 // validación de path absoluta o relativa
 // validación de directorio
 // validación de archivo md
 
-}
 
-module.exports = { mdLinks }
+
+module.exports = {
+    mdLinks,
+    validatePath,
+    validateOption
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,25 +1,57 @@
-const mdLinks = require('../src/index.js');
+const {
+  mdLinks,
+  validatePath,
+  validateOption,
+} = require('../src/index.js');
 
-describe('mdLinks', () => {
+const validPath = './files-to-read/achicando.md';
+const invalidPath = 'achicando'
+
+//test para mdLinks
+describe('Test para mdLinks', () => {
+  it('mdLinks debería ser una función', () => {
+    expect(typeof mdLinks).toBe('function')
+  })
+});
+
+// test para validacion de path
+describe('Test para validar la ruta (path)', () => {
+  test('Retorna true si la ruta es valida', () => {
+    return expect(validatePath(validPath)).resolves.toBe(true)
+  });
+  test('Arroja error si la ruta es invalida', () => {
+    return expect(validatePath(invalidPath)).rejects.toThrow()
+  });
+});
+
+// test para validación de option
+describe('Test para validar la opcion (option)', () => {
+  test('Retorna true si la opcion es valida', () => {
+    return expect(validateOption({ validate: true })).resolves.toBe(true)
+  });
+  test('Retorna true si la opcion es valida', () => {
+    return expect(validateOption({ validate: false })).resolves.toBe(true)
+  });
+  test('Retorna true si la opcion es valida', () => {
+    return expect(validateOption('validate')).resolves.toBe(true)
+  });
+  test('Arroja error si la opcion es invalida', () => {
+    return expect(validateOption('option')).rejects.toThrow()
+  });
+  test('Arroja error si la opcion es invalida', () => {
+    return expect(validateOption({ validate: 33 })).rejects.toThrow()
+  });
+});
+
+
+// test para validación de path absoluta o relativa
+// test para validación de directorio
+// test para validación de archivo md
+//------------------------------------
+/* describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
   });
 
-});
-
-// test para validacion de path
-
-describe('Test para validar la ruta (path)', () => {
-  it('Retorna true si la ruta es valida', () => {
-    expect(validatePath(path)).resolve.toEqual(true);
-  });
-  it('Retorna false si la ruta es invalida', () => {
-    expect(validatePath(path)).reject.toEqual(false);
-  });
-});
-
-// test para validación de option
-// test para validación de path absoluta o relativa
-// test para validación de directorio
-// test para validación de archivo md
+}); */
