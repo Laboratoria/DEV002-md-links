ff05d33fd4ec029ab0409ec50f51cbd29d953707
Sandra-18 <sanlorenita-18@hotmail.com>
2023-02-21 14:53:18 -0500

    se inicia la primera parte del test de mdLink

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ const mdLink = (path, options) => {
    * @param reject rechaza si la promesa cuando no se cumple
    */
   return new Promise((resolve, reject) => {
-    /**
-     * !option[] no puede estar undefined
-     */
-    if (options[0] === undefined && options[1] === undefined) {
-      reject('invalid route');
-    } else {
-      routExist(path);
-      resolve('the route exists')
-    }
   });
 };
 
