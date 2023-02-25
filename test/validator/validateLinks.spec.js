88821a38aa6fd5af9aa18498b8601ad225f94db5
Sandra-18 <sanlorenita-18@hotmail.com>
2023-02-25 17:29:42 -0500

    se modifica el test de la funcion validateLinks, ya no se le pasa el objeto, si no una simulacion del objeto de axios

--- test/validator/validateLinks.spec.js
+++ test/validator/validateLinks.spec.js
@@ -7,20 +7,22 @@ beforeEach(() => {
 });
 describe("everything related to the validateLinks function", () => {
   it("sould return an array of the objects", () => {
-    
-    const containerArrayObjects = [{
-      href: "https://curriculum.laboratoria.la/es/topics/css/01-css/01-intro-css",
+    const containerArrayObjects = [
+      {
+        href: "https://curriculum.laboratoria.la/es/topics/css/01-css/01-intro-css",
+        status: 200,
+        ok: "OK",
+      },
+    ];
+    axios.get.mockResolvedValueOnce({
       status: 200,
-      ok: "OK",
-    }];
-    axios.get.mockResolvedValueOnce(containerArrayObjects[0]);
+      statusText: "OK",
+    });
     validateLinks(containerArrayObjects).then((data) => {
       expect(data).toHaveLength(1);
+      expect(data[0].status).not.toBe(404)
     });
   });
-
-
-
 
   it("soult return a status 404", () => {
     const arrayObject404 = [
@@ -33,9 +35,10 @@ describe("everything related to the validateLinks function", () => {
     axios.get.mockResolvedValueOnce(arrayObject404[0]);
 
     validateLinks(arrayObject404).then((data) => {
-      data.forEach((e) => {  // debo recorrer el contenido del objeto para acceder a status
-        expect(e.status).not.toBe(200)
-      })
+      data.forEach((e) => {
+        // debo recorrer el contenido del objeto para acceder a status
+        expect(e.status).not.toBe(200);
+      });
     });
   });
 });
