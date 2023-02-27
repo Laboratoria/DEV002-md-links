67330ee6f085b1f3cb7406a38edcca9c5d829541
Sandra-18 <sanlorenita-18@hotmail.com>
2023-02-27 18:49:06 -0500

    se termina la covertura de mdLinks en la parte de --validate

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const { getAllMdFiles, validateRoute, returnBrokenLinks, validateLinks, statUnique } = require("./validator");
+const {
+  getAllMdFiles,
+  validateRoute,
+  returnBrokenLinks,
+  validateLinks,
+  statUnique,
+} = require("./validator");
 
 /**
  *
@@ -10,62 +16,44 @@ const { getAllMdFiles, validateRoute, returnBrokenLinks, validateLinks, statUniq
 const mdLink = (path, options) => {
   return new Promise((resolve, reject) => {
     if (options[0] === undefined && options[1] === undefined) {
-      console.log("no se ha especificado la opcion");
       const pathnameArray = getAllMdFiles(path);
       pathnameArray.forEach((elem) => {
         validateRoute(elem)
           .then((data) => {
             resolve(data);
-            if (Object.keys(data).length === 0) {
-              console.log("tu array no tiene links");
-            } else {
-              console.log(data, "estos son tu datos");
-            }
           })
-          .catch((error) => {
-            reject(error);
-          });
+          .catch((error) => reject(error));
       });
     } else {
       if (
         (options[0] === "--validate" && options[1] === "--stats") ||
         (options[0] === "--stats" && options[1] === "--validate")
       ) {
-        console.log("si hay una opcion");
+        const routArray = getAllMdFiles(path);
+        routArray.forEach((elem) => {
+          validateRoute(elem)
+            .then((data) => {
+              resolve(returnBrokenLinks(data));
+            })
+        });
+      } else if (options[0] === "--validate") {
+        const routArray = getAllMdFiles(path);
+        routArray.forEach((elem) => {
+          validateRoute(elem)
+            .then((data) => {
+              if (Array.isArray(data)) {
+                validateLinks(data).then((data) => resolve(data));
+              }
+            })
+            .catch((error) => reject(error));
+        });
+      } else if (options[0] === "--stats") {
         const routArray = getAllMdFiles(path);
         routArray.forEach((elem) => {
           validateRoute(elem).then((data) => {
-            resolve(returnBrokenLinks(data));
-            if(Object.keys(data).length === 0){
-              console.log("tu archivo md no tiene links");
-            }else{
-              console.log(returnBrokenLinks(data))
-            }
-          }).catch((error) => {
-              reject(error);
-          })
-        })
-      }else if(options[0] === "--validate"){
-        console.log("hay validacion");
-        const routArray = getAllMdFiles(path)
-        routArray.forEach((elem) => {
-            validateRoute(elem).then((data) => {
-              validateLinks(data).then((data) => {
-                  console.log(data);
-              }).catch((error) => {
-                reject(error)
-              })
-            })
-        })
-      }else if(options[0] === "--stats"){
-        console.log("hay validacion");
-        const routArray = getAllMdFiles(path)
-        routArray.forEach((elem) => {
-            validateRoute(elem).then((data) => {
-              resolve(statUnique(data))
-              console.log(statUnique(data));
-            })
-        })
+            resolve(statUnique(data));
+          });
+        });
       }
     }
   });
