d9c595127671d018d193d7535e3d76d66199c1ca
Roxsy <roxsy.dj@gmail.com>
2023-03-02 09:08:25 -0500

    progress funcion

--- option.js
+++ option.js
@@ -1,18 +1,3 @@
-const { mdLinks } = require('./promise.js');
 
-const md_Links = (pathname, options) => {
-    if ((options === null || options === undefined) || options === '{ validate: false }' || options.validate === false/*&& (secondOption === null || firstOption === undefined)*/) {
-        
-        mdLinks(pathname, '')
-            .then(links => {
-                console.log(links);
-            })
-    } else if (options === '{ validate: true }' || options.validate === true) {
 
-        mdLinks(pathname, ['--validate'])
-            .then(outcome => {
-                console.log(outcome);
-            })
-    }
-}
 
